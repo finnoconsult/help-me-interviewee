a25e9100591cccb1cc881087925d93eb6a9740da
Levente Mathe <levente.l.mathe@gmail.com>
2021-11-17 17:12:07 +0100

    better usage of node-sqlite

--- apps/service/src/main.ts
+++ apps/service/src/main.ts
@@ -10,24 +10,18 @@ const port = process.env.port || 3000;
 app.use(bodyParser.json());
 
 app.get('/starships', (req, res) => {
-  const ships = [];
-  db.each(
-    'SELECT * FROM starship;',
-    (err, row) => ships.push(row),
-    () => res.send({ ships })
-  );
+  db.all('SELECT * FROM starship;', (err, rows) => res.send({ ships: rows }));
 });
 
 app.get('/starships/:id', (req, res) => {
   let ship = null;
-  const stmt = db.prepare('SELECT * FROM starship WHERE id=?;');
-  stmt.each(
-    req.params.id,
-    (err, row) => {
+  db.serialize(() => {
+    const stmt = db.prepare('SELECT * FROM starship WHERE id=?;');
+    stmt.get(req.params.id, (err, row) => {
       ship = row;
-    },
-    () => stmt.finalize(() => res.send({ ship }))
-  );
+    });
+    stmt.finalize(() => res.send({ ship }));
+  });
 });
 
 app.post('/starships/race', (req, res) => {
@@ -51,56 +45,50 @@ app.use('*', (req, res) => {
 });
 
 const server = app.listen(port, async () => {
-  db.run(
-    `CREATE TABLE starship (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(255),
-    cost_in_credits DOUBLE,
-    max_atmosphering_speed DOUBLE
-  );`,
-    async () => {
-      let starships = [];
-      let next = `https://swapi.dev/api/starships?page=1`;
+  let starships = [];
+  let next = `https://swapi.dev/api/starships?page=1`;
 
-      while (next !== null) {
-        const result = (await axios.get(next)) as any;
-        starships = starships.concat(
-          result.data.results.filter(
-            (ship) =>
-              !Number.isNaN(Number(ship.cost_in_credits)) &&
-              !Number.isNaN(Number(ship.max_atmosphering_speed))
-          )
-        );
-        next = result.data.next;
-      }
+  while (next !== null) {
+    const result = (await axios.get(next)) as any;
+    starships = starships.concat(
+      result.data.results.filter(
+        (ship) =>
+          !Number.isNaN(Number(ship.cost_in_credits)) &&
+          !Number.isNaN(Number(ship.max_atmosphering_speed))
+      )
+    );
+    next = result.data.next;
+  }
 
-      console.log(
-        'Found ships with good data: ',
-        starships.map((ship) => ship.name)
-      );
-
-      const stmt = db.prepare(
-        'INSERT INTO starship (name, cost_in_credits, max_atmosphering_speed) VALUES (?, ?, ?);'
-      );
-      starships.forEach((ship) => {
-        stmt.run([
-          ship.name,
-          ship.cost_in_credits,
-          ship.max_atmosphering_speed,
-        ]);
-      });
-      stmt.finalize(() => {
-        console.log('Seeded ships: ');
-        db.each(
-          'SELECT * from STARSHIP;',
-          (err, row) => console.log(row),
-          () => {
-            console.log(`Listening at http://localhost:${port}`);
-          }
-        );
-      });
-    }
+  console.log(
+    'Found ships with good data: ',
+    starships.map((ship) => ship.name)
   );
+
+  db.serialize(async () => {
+    db.run(
+      `CREATE TABLE starship (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name VARCHAR(255),
+      cost_in_credits DOUBLE,
+      max_atmosphering_speed DOUBLE
+    );`
+    );
+
+    const stmt = db.prepare(
+      'INSERT INTO starship (name, cost_in_credits, max_atmosphering_speed) VALUES (?, ?, ?);'
+    );
+    starships.forEach((ship) => {
+      stmt.run([ship.name, ship.cost_in_credits, ship.max_atmosphering_speed]);
+    });
+    stmt.finalize();
+
+    db.all('SELECT * from STARSHIP;', (err, rows) => {
+      console.log('Seeded ships: ');
+      console.log(rows);
+      console.log(`Listening at http://localhost:${port}`);
+    });
+  });
 });
 
 server.on('error', console.error);
