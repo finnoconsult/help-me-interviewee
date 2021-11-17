aac580fea5fea8e5dfc476d58b95cd76652c2053
Levente Mathe <levente.l.mathe@gmail.com>
2021-11-17 11:40:38 +0100

    setup

--- apps/service/src/main.ts
+++ apps/service/src/main.ts
@@ -1,18 +1,13 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import * as express from 'express';
 
 const app = express();
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.send({ message: 'Welcome to service!' });
 });
 
-const port = process.env.port || 3333;
+const port = process.env.port || 3000;
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Listening at http://localhost:${port}`);
 });
 server.on('error', console.error);
